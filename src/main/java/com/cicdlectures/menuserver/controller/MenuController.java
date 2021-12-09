4e3f62b860e36093deb3a939ca4bf3cc00324378
NRouyer38 <N.rouyer38@gmail.com>
2021-12-09 12:42:45 +0000

    add deleteMenus with id

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -3,8 +3,13 @@ package com.cicdlectures.menuserver.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -23,10 +28,13 @@ public class MenuController {
 
   private final ListMenuService listMenuService;
 
+  private final MenuRepository menuRepository;
+
   @Autowired
-  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService) {
+  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService, MenuRepository menuRepository) {
     this.createMenuService = createMenuService;
     this.listMenuService = listMenuService;
+    this.menuRepository = menuRepository;
   }
 
   @GetMapping(path = "/menus", produces = "application/json")
@@ -40,8 +48,8 @@ public class MenuController {
     return createMenuService.createMenu(menu);
   }
 
-  @DeleteMapping("/menus/{id}")
-  void deleteEmployee(@PathVariable Long id) {
-    repository.deleteById(id);
+  @DeleteMapping(path = "/menus/{id}")
+  void deleteMenus(@PathVariable Long id) {
+    menuRepository.deleteById(id);
   }
 }

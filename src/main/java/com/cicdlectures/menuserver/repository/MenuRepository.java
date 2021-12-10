3f32c76522abda999690858f270bb6c630afd5fa
NRouyer38 <N.rouyer38@gmail.com>
2021-12-10 10:53:25 +0000

    add integration test in MenuControllerIT

--- src/main/java/com/cicdlectures/menuserver/repository/MenuRepository.java
+++ src/main/java/com/cicdlectures/menuserver/repository/MenuRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import com.cicdlectures.menuserver.model.Menu;
 
 public interface MenuRepository extends CrudRepository<Menu, Long> {
-
 }
--- src/test/java/com/cicdlectures/menuserver/controller/MenuControllerIT.java
+++ src/test/java/com/cicdlectures/menuserver/controller/MenuControllerIT.java
@@ -1,3 +1,41 @@
+package com.cicdlectures.menuserver.service;
+
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.Arrays;
+
+import com.cicdlectures.menuserver.dto.MenuDto;
+import com.cicdlectures.menuserver.dto.DishDto;
+import com.cicdlectures.menuserver.model.Menu;
+import com.cicdlectures.menuserver.model.Dish;
+import com.cicdlectures.menuserver.repository.MenuRepository;
+import com.cicdlectures.menuserver.repository.DishRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 // src/test/java/com/cicdlectures/menuserver/controller/MenuControllerIT.java
 // Lance l'application sur un port aléatoire.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,5 +54,23 @@ public class MenuControllerIT {
   @Test
   @DisplayName("lists all known menus")
   public void listsAllMenus() throws Exception {
+  }
+
+    // Injecte automatiquement l'instance du menu repository
+  @Autowired
+  private MenuRepository menuRepository;
+
+  // Injecte automatiquement l'instance du TestRestTemplate
+  @Autowired
+  private TestRestTemplate template;
+
+  public void listExitingMenus() throws Exception {
+    // Effectue une requête GET /menus
+    ResponseEntity<MenuDto[]> response = this.template.getForEntity(getMenusURL().toString(), MenuDto[].class);
+
+    //Parse le payload de la réponse sous forme d'array de MenuDto
+    MenuDto[] gotMenus = response.getBody();
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
   }
 }
